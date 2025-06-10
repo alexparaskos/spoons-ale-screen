22df68b5ab896d1bee5e348f63c9328cc2cbea9a
alexparaskos <alex.paraskos@me.com>
2025-06-10 02:45:29 +0100

    dist

--- dist/assets/md.transition-legacy-BflOX2ET.js
+++ dist/assets/md.transition-legacy-BflOX2ET.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-5HHbt0f4.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-Llg8c5iM.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
