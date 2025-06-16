aebcb240c03403bc081cb4a52b091dd691bb179b
alexparaskos <alex.paraskos@me.com>
2025-06-16 14:02:29 +0100

    rm err in post

--- dist/assets/ios.transition-legacy-C0_fI8zs.js
+++ dist/assets/ios.transition-legacy-C0_fI8zs.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DGYRMH-z.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DCwAw9Wj.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- dist/assets/status-tap-legacy-DkbCzb1H.js
+++ dist/assets/status-tap-legacy-DkbCzb1H.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DGYRMH-z.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-DCwAw9Wj.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
