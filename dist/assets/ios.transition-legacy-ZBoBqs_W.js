397ec6aadb1f722dc51f2ee948d0e2dd316235d5
alexparaskos <alex.paraskos@me.com>
2025-06-16 14:12:37 +0100

    fix regex error

--- dist/assets/ios.transition-legacy-ZBoBqs_W.js
+++ dist/assets/ios.transition-legacy-ZBoBqs_W.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DCwAw9Wj.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-wbJ8n3EA.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
