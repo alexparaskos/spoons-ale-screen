bb169bf75a1defeeb0ba94a462b296f3ee3252a1
alexparaskos <alex.paraskos@me.com>
2025-09-25 16:51:17 +0100

    footer change - pre swiper

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -106,10 +106,10 @@ const HorizontalStickyFooter: React.FC = () => {
                   } else if (ale.colour_code == "5") {
                     color = "ale-dark"
                   } return (
-                    <div className='ale-list-item' key={i}>
+                    <div className='ale-list-item pl-16' key={i}>
                       <div className='ion-margin-vertical'>
                         <h5>
-                          <span>{ale.brewery} {ale.product}</span>
+                          <span> <b>{ale.brewery}</b> {ale.product}</span>
                           <span className='menu-item-icons'>
                             <span className="menu-item-icon d-flex ion-align-items-center"><IonIcon icon="data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' class='ionicon' viewBox='0 0 512 512'><path d='M256 464c-114.69 0-208-93.31-208-208S141.31 48 256 48s208 93.31 208 208-93.31 208-208 208z'/></svg>" color={color} className={"menu-item-icon_icon menu-item-icon_icon-" + color + "md ion-color ion-color-" + color} role="img"></IonIcon></span>
                           </span>
