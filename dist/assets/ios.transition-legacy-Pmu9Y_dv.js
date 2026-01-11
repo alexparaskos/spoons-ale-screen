4192f7664c0499de7cdd641300f592ae01af8e53
alexparaskos <alex.paraskos@me.com>
2026-01-11 18:23:16 +0000

    added new ciders

--- dist/assets/ios.transition-legacy-Pmu9Y_dv.js
+++ dist/assets/ios.transition-legacy-Pmu9Y_dv.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dwr--EVm.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BXPtuA8U.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
